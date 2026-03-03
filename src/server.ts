7270f49d802e9c2b8dbd978fad1347ff9d612542
Madi25-lab <Madic2507@gmail.com>
2026-03-03 13:14:06 -0500

    Fronted listo para produccion

--- src/server.ts
+++ src/server.ts
@@ -78,13 +78,9 @@ app.post("/api/ventas", (req, res) => {
 });
 
 // ===== FRONTEND =====
-app.use(express.static("public" ));
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    res.send("Servidor funcionando correctamente ");
-});
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en puerto " + PORT);
