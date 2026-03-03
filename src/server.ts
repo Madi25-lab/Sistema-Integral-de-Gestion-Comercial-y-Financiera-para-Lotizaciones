3100bfa073128b4d09fa0a9defadac46a248724b
Madi25-lab <Madic2507@gmail.com>
2026-03-03 13:32:41 -0500

    Agregada ruta raiz para produccion

--- src/server.ts
+++ src/server.ts
@@ -95,6 +95,10 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>{
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando correctamente 🚀");
+});
+
+app.listen(PORT, () => {
     console.log("Servidor corriendo en puerto " + PORT);
 });
